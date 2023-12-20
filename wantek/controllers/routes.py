c9fe193144d198a05c6dbb07a3e5eab5f234381b
Benny <benny.kurniawan@sat.co.id>
2023-12-20 15:06:38 +0700

    Perubahan navbar dinamis by level menu

--- wantek/controllers/routes.py
+++ wantek/controllers/routes.py
@@ -12,37 +12,22 @@ def home():
 def global_var():
     # Function global var for Jinja2
 
-    # Set akses menu untuk navbar
-    organized_menu = {}
+    # Set akses menu untuk navbar    
+    list_akses_menu = []
     if (current_user.is_authenticated):
         hasil_get_list_akses_menu   = get_list_akses_menu(current_user.role)
         if (hasil_get_list_akses_menu["status"] == "F"):
             message     = hasil_get_list_akses_menu["message"]
             flash_type  = "error"
             flash(message, flash_type)
-        list_akses_menu     = hasil_get_list_akses_menu["result"] 
-        # Set children dari menu
-        grandparent = list_akses_menu[0]["m_id"]
-        for menu in list_akses_menu:
-            menu_id, menu_desc, menu_parent, menu_link, menu_level = menu.values()            
-            if (menu_level < 1):
-                grandparent = menu_id
-                organized_menu[menu_id] = {"desc": menu_desc, "link": menu_link, "level": menu_level, "children": []}
-            else:
-                if (menu_parent in organized_menu):
-                    organized_menu[menu_parent]["children"].append({"id": menu_id, "desc": menu_desc, "link": menu_link, "grandchildren": []})
-                # Set grandchildren
-                else:       
-                    for child in organized_menu[grandparent]["children"]:
-                        if (child["id"] == menu_parent):
-                            child["grandchildren"].append({"id": menu_id, "desc": menu_desc, "link": menu_link})        
+        list_akses_menu     = hasil_get_list_akses_menu["result"]         
 
     return dict(
         APP_NAME    = app.config["APP_NAME"],
         APP_INITIAL = app.config["APP_INITIAL"],
         COPYRIGHT   = app.config["COPYRIGHT"],
-        VERSION     = app.config["VERSION"],
-        AKSES_MENU  = organized_menu
+        VERSION     = app.config["VERSION"],        
+        AKSES_MENU  = list_akses_menu
     )
 
 @app.errorhandler(403)
